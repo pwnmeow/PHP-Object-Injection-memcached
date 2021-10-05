37eca9726bf62aa6b8eb5a8d8cd52cfb13ab2dd3
pwnmeow <hacker.ascool@gmail.com>
2021-10-05 20:37:01 +0000

    perfect match

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-sudo docker run --publish=80:80 --publish=11211:11211 -ti -v /var/run/docker.sock:/var/run/docker.sock cloudapp
+sudo docker run --restart always --detach --name cloudapp-auto-restart --publish=80:80 --publish=11211:11211 -ti -v /var/run/docker.sock:/var/run/docker.sock cloudapp
+
